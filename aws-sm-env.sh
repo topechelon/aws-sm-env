d57cd1e0c9b40a9ffdbbba511bd25132cf59e567
Michael McHolm <mmcholm@patriotsoftware.com>
2018-10-05 11:16:28 -0400

    aws sm env support output to file

--- aws-sm-env.sh
+++ aws-sm-env.sh
@@ -1,8 +1,23 @@
 #!/bin/sh
-OUTPUT=$(aws-sm-env $AWS_SM_ENV_OPTIONS)
-if [ "$?" -eq "0" ];then
-  eval $OUTPUT
-  exec "$@"
-else
-  echo $OUTPUT
+if [ "$AWS_SM_ENV_OPTIONS" != "" ];then
+  ENV_OUTPUT=$(aws-sm-env $AWS_SM_ENV_OPTIONS)
+  ENV_RC=$?
+  if [ "$ENV_RC" -eq "0" ];then
+    eval $ENV_OUTPUT
+  else
+    echo $ENV_OUTPUT
+    exit 1
+  fi
 fi
+if [ "$AWS_SM_FILE" != "" -a "$AWS_SM_FILE_OPTIONS" != "" ];then
+  FILE_OUTPUT=$(aws-sm-env $AWS_SM_FILE_OPTIONS)
+  echo $FILE_OUTPUT
+  FILE_RC=$?
+  if [ "$FILE_RC" -eq "0" ];then
+    echo $FILE_OUTPUT > $AWS_SM_FILE
+  else
+    echo $FILE_OUTPUT
+    exit 1
+  fi
+fi
+exec "$@"
